8d0516e6f7212c8ccdfb3ac9090edfc195f42301
Carl Ripa <cj.ripa@gmail.com>
2016-04-03 15:06:18 +0200

    Added some design to the frontpage and also the mottos fetched from DB.

--- src/UniQode.Web/Controllers/HomeController.cs
+++ src/UniQode.Web/Controllers/HomeController.cs
@@ -1,18 +1,46 @@
-﻿using System.Web.Mvc;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using UniQode.Contracts.Services;
+using UniQode.Entities.Data;
+using UniQode.Models.Shared;
 
 namespace UniQode.Web.Controllers
 {
     public class HomeController : Controller
     {
-        public HomeController()
+        public HomeController(IMultiTenantService<Motto, Guid> mottoService)
         {
-
+            _mottoService = mottoService;
         }
+
+        private readonly IMultiTenantService<Motto, Guid> _mottoService;
 
         // GET: Home
         public ActionResult Index(bool preview = false)
         {
             return View();
+        }
+
+        [ChildActionOnly]
+        [HttpGet]
+        public ActionResult Mottos(bool preview = false)
+        {
+            ICollection<Motto> dtos = null;
+
+            if (preview)
+            {
+                dtos = _mottoService.Secondary.List(true);
+            }
+            else
+            {
+                dtos = _mottoService.Primary.List(true);
+            }
+
+            var models = dtos.Select(MottoModel.FromDto).ToList();
+
+            return PartialView("Partials/_Mottos", models);
         }
     }
 }
