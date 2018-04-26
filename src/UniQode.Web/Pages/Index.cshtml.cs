e1c0547841a9968baa098233edb74cec7daeccfd
Carl Ripa <cj.ripa@gmail.com>
2018-04-26 18:28:51 +0200

    Basic project structure implemented

---
+++ src/UniQode.Web/Pages/Index.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace UniQode.Web.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
